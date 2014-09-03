2050e4960fc629abf6d7aa201903a8738214af28
Eduardo Ramírez <ejramire@uc.cl>
2014-09-03 15:37:56 -0400

    sample

--- Ramirez_Eduardo_Tarea1.py
+++ Ramirez_Eduardo_Tarea1.py
@@ -3,6 +3,19 @@ from mpi4py import MPI
 class E_Ramirez():
     def sample_sort(self, elements, numberOfProcesses, s, m):
         #firstly, get separators
+        numberOfElements = len(elements)
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+        #every process gets an equal size subarray from where to get a sample
+        my_sample = []
+        fraction = numberOfElements/numberOfProcesses
+        bottom = int(fraction*rank)
+        if (rank == numberOfProcesses - 1):
+            top = numberOfElements
+        else:
+            top = int(fraction*(rank + 1))
+        for e in elements[bottom:top]:
+            pass
         return elements
 
     def sparse_graph_sort(self, matrix, numberOfProcesses):
@@ -14,7 +27,7 @@ class E_Ramirez():
     def bucket_sort(self, elements, numberOfBuckets, a, b):
         comm = MPI.COMM_WORLD
         rank = comm.Get_rank()
-        mybucket = []
+        my_bucket = []
         fraction = (b-a)/numberOfBuckets
         bottom = a + int(fraction*rank)
         if (rank == numberOfBuckets - 1):
@@ -23,9 +36,9 @@ class E_Ramirez():
             top = a + int(fraction*(rank + 1))
         for e in elements:
             if e >= bottom and e < top:
-                mybucket.append(e)
-        mybucket.sort()
-        return comm.gather(mybucket, root=0)
+                my_bucket.append(e)
+        my_bucket.sort()
+        return comm.gather(my_bucket, root=0)
 
 
 
