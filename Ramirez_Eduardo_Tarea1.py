6c8c4c53f385d5127f7aaa45d01c3033d9ec7d7e
Eduardo Ramírez <ejramire@uc.cl>
2014-09-03 22:22:10 -0400

    working until now

--- Ramirez_Eduardo_Tarea1.py
+++ Ramirez_Eduardo_Tarea1.py
@@ -1,21 +1,41 @@
 from mpi4py import MPI
+import numpy
 
 class E_Ramirez():
-    def sample_sort(self, elements, numberOfProcesses, s, m):
+    def sample_sort(self, elements, number_of_processes, s, m):
         #firstly, get separators
-        numberOfElements = len(elements)
+        number_of_elements = len(elements)
         comm = MPI.COMM_WORLD
         rank = comm.Get_rank()
+        last_rank = number_of_processes - 1
         #every process gets an equal size subarray from where to get a sample
-        my_sample = []
-        fraction = numberOfElements/numberOfProcesses
-        bottom = int(fraction*rank)
-        if (rank == numberOfProcesses - 1):
-            top = numberOfElements
+        fraction = number_of_elements/number_of_processes
+        bottom = int(fraction * rank)
+        if (rank == number_of_processes - 1):
+            top = number_of_elements
         else:
             top = int(fraction*(rank + 1))
-        for e in elements[bottom:top]:
-            pass
+        num_elements_to_select = int(s/number_of_processes)
+        if(s%number_of_processes > rank):
+            num_elements_to_select += 1
+        my_sample = numpy.random.choice(elements[bottom:top], num_elements_to_select, replace=True)
+        my_sample.sort()
+        comm.gather(my_sample, root=last_rank)
+        #select m-1 separators
+        my_separators = []
+        if(rank == last_rank):
+            my_sample.sort()
+            sub_arrays = numpy.array_split(my_sample, m)
+            separators = [x[-1] for x in sub_arrays]
+            my_separators = [separators[-2], -1]
+            #send message to the rest of the processes
+            for i in range(last_rank):
+                to_send = [separators[i], separators[i+1]]
+                comm.send(to_send, dest=i)
+            print (rank, ' : send all. My separators are ', my_separators)
+        else:
+            my_separators = comm.recv(source=last_rank)
+            print (rank, ' : Received. My separators are ', my_separators)
         return elements
 
     def sparse_graph_sort(self, matrix, numberOfProcesses):
--- test.py
+++ test.py
@@ -3,27 +3,34 @@ from Ramirez_Eduardo_Tarea1 import E_Ramirez
 from mpi4py import MPI
 import time
 
-def test_bucket(numberOfProcesses, rank, numberOfElements):
+def test_bucket(number_of_processes, rank, number_of_elements):
     testInstance = E_Ramirez()
     if (rank == 0):
         start = time.clock()
-    testInstance.bucket_sort(np.random.randint(1,1000000,numberOfElements), numberOfProcesses, 1, 1000000)
+    testInstance.bucket_sort(np.random.randint(1,1000000,number_of_elements), number_of_processes, 1, 1000000)
     if (rank == 0):
         end = time.clock()
         return '%.5f' % (end - start)
 
-def test_bucket_all(numberOfProcesses, rank):
+def test_bucket_all(number_of_processes, rank):
     if (rank == 0):
-        print('Bucket ', numberOfProcesses, ' processes '),
+        print('Bucket ', number_of_processes, ' processes '),
     results = []
-    results.append(test_bucket(numberOfProcesses, rank, 1000))
-    results.append(test_bucket(numberOfProcesses, rank, 10000))
-    results.append(test_bucket(numberOfProcesses, rank, 100000))
-    results.append(test_bucket(numberOfProcesses, rank, 1000000))
+    results.append(test_bucket(number_of_processes, rank, 1000))
+    results.append(test_bucket(number_of_processes, rank, 10000))
+    results.append(test_bucket(number_of_processes, rank, 100000))
+    results.append(test_bucket(number_of_processes, rank, 1000000))
     if (rank == 0):
         print(' '.join(results))
-def test_sample():
-    pass
+def test_sample(number_of_processes, rank , number_of_elements, s, m):
+    testInstance = E_Ramirez()
+    if (rank == 0):
+        start = time.clock()
+    testInstance.sample_sort(np.random.randint(1,1000000,number_of_elements), number_of_processes, s, m)
+    if (rank == 0):
+        end = time.clock()
+        return '%.5f' % (end - start)
+
 def test_sparse():
     pass
 def test_shortest():
@@ -31,9 +38,9 @@ def test_shortest():
 
 if __name__ == "__main__":
     comm = MPI.COMM_WORLD
-    numberOfProcesses = comm.Get_size()
+    number_of_processes = comm.Get_size()
     rank = comm.Get_rank()
-    test_bucket_all(numberOfProcesses, rank)
-    test_sample()
+    #test_bucket_all(number_of_processes, rank)
+    test_sample(number_of_processes, rank, 10000, 100, 5)
     test_sparse()
     test_shortest()
