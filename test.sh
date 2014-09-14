51d126efac6d0baa945e3f6948d32f9648693625
Eduardo Ramírez <ejramire@uc.cl>
2014-09-14 19:53:11 -0300

    removed unnecesary files

--- test.sh
+++ test.sh
@@ -1,8 +1,4 @@
-mpiexec -np 1 python3 test.py
-mpiexec -np 2 python3 test.py
-mpiexec -np 3 python3 test.py
-mpiexec -np 4 python3 test.py
-mpiexec -np 5 python3 test.py
-mpiexec -np 6 python3 test.py
-mpiexec -np 7 python3 test.py
-mpiexec -np 8 python3 test.py
+mpiexec -np 40 python3 test_bucket.py
+mpiexec -np 40 python3 test_sample.py
+mpiexec -np 40 python3 test_shortest.py
+mpiexec -np 40 python3 test_sparse.py
